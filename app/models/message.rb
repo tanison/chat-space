d42ecd2334aac23ae27dbfa77485e44ac61944ef
tanison <tanison949@icloud.com>
2019-05-09 14:55:16 +0900

    message-ver1.1
    問５のメッセージを投稿まで終了。
    フォームの部分は答えを流用した。

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ApplicationRecord
-  berongs_to :group
-  berongs_to :user
+  belongs_to :group
+  belongs_to :user
 
   validates :content, presence: true, unless: :image?
 
-  mount_uploader :image, ImagesUploader
+  # mount_uploader :image, ImagesUploader
 end
