9282b59f2719bb47481e592559dcef47bddc04ee
tanison <tanison949@icloud.com>
2019-05-15 20:29:07 +0900

    add-ver1.1
    2個取得して追加する方式

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -4,7 +4,7 @@ $(function() {
 
   function appendUser(user) {
     var html =
-                `<div class="chat-group-user clearfix">
+               `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
                 <div class="user-search-add chat-group-user__btn chat-group-user__btn--add", id="user-add" data-user-id=${user.id} data-user-name="${user.name}">追加</div>
                 </div>`
@@ -20,14 +20,17 @@ $(function() {
     search_list.append(html);
   }
 
-  function buildHTML(user){
-    var html =
-            `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-            <input name='group[user_ids][]' type='hidden' value='${user.id}'>
-            <p class='chat-group-user__name'>${user.name}</p>
-            <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-          </div>`
-          return html;
+  // var search_list = $(".chat-group-users.clearfix.js-chat-member");
+
+  function buildHTML(user_name, user_id){
+    debugger
+    var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+              <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+              <p class='chat-group-user__name'>${user_name}</p>
+              <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+              </div>`
+          console.log(9)
+    return html;
   }
 
   $(".user__input").on("keyup", function() {
@@ -65,7 +68,11 @@ $(function() {
     // onの第1引数にイベント名を
     // 第2引数に設定対象のid等を記述する
     $(document).on("click", "#user-add", function() {
-      var html = buildHTML();
+      var user_id = $(this).data("user-id");
+      var user_name = $(this).data("user-name");
+      var html = buildHTML(user_name, user_id);
+      $('.chat-group-users.clearfix.js-add-user').append(html);
+      console.log(7)
     });
 
     // ここで追加したコントロールにもイベントが設定される
