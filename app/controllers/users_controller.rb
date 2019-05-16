5b89ef65c896f162a60550c4b59a691db817ab4b
tanison <tanison949@icloud.com>
2019-05-16 21:25:43 +0900

    ver1.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:name]}%")
-    # @users = User.where('name LIKE(?) AND id != ?', "%#{params[:name]}%", current_user)
+    # @users = User.where('name LIKE(?)', "%#{params[:name]}%")
+    @users = User.where('name LIKE(?) AND id != ?', "%#{params[:name]}%", current_user)
     respond_to do |format|
       format.html
       format.json
