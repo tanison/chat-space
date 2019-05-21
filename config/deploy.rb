b8ed2d77d4ed328c7c41ac11625626a9abdb0ebf
tanison <tanison949@icloud.com>
2019-05-21 21:31:57 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url,  'git@github.com:tanison/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.５.1'
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/.ssh/awsfirst.pem']
