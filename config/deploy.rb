132add523e255657e47474c0f3bdaeb672eb0bf4
tanison <tanison949@icloud.com>
2019-05-22 17:56:13 +0900

    ver2.3

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/.ssh/awsfirst.pem']
+                  keys: ['/.ssh/aws_3.pem']
                   # ※例：~/.ssh/key_pem.pem
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
